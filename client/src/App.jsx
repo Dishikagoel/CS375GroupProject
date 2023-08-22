05ae3f6d56b8ef5d233970153b71d6af63985440
dishika.goel <dishika.goel@drexel.edu>
2023-08-22 14:52:07 -0400

    Add a login page, and update the index page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import UserInfo from './pages/userInfo';
 import Signup from './pages/signup';
+import Login from './pages/login';
 
 function App() {
 
-  return (
-    <Router>
-      <div className='App'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/userInfo" element={<UserInfo />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </Router>
-  )
+    return (
+        <Router>
+            <div className='App'>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/userInfo" element={<UserInfo />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,6 +1,7 @@
-import { Typography, AppBar, CssBaseline, Container, Toolbar, Button } from '@mui/material';
+import { Typography, AppBar, CssBaseline, Container, Button, Toolbar, Grid, Paper } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { cyan, purple } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
 import AppBarr from '../../components/appbar';
 
 const theme = createTheme({
@@ -16,16 +17,28 @@ const Home = () => {
             <CssBaseline />
             <AppBarr />
             <main>
-                <div>
-                    <Container maxWidth="sm">
-                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                <Container maxWidth="md" sx={{ mt: 5 }}>
+                    <Paper elevation={3} sx={{ p: 4, textAlign: 'center' }}>
+                        <Typography variant="h2" color="primary" gutterBottom>
                             Welcome to WeBay
                         </Typography>
-                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                            The biggest online aunction site in the world!
+                        <Typography variant="h5" color="textSecondary" paragraph>
+                            The biggest online auction site in the world!
                         </Typography>
-                    </Container>
-                </div>
+                        <Grid container spacing={2} justifyContent="center">
+                            <Grid item>
+                                <Button variant="contained" color="secondary" component={Link} to="/signup">
+                                    Sign Up
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="contained" color="secondary" component={Link} to="/login">
+                                    Log In
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Container>
             </main>
         </ThemeProvider>
     );
