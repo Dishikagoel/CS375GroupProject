d95161d2385a12685c7523c419283cb51ce76c24
Shravya0 <81495955+Shravya0@users.noreply.github.com>
2023-08-22 11:53:37 -0400

    fix useNavigate button

--- client/src/components/appbar.tsx
+++ client/src/components/appbar.tsx
@@ -1,17 +1,16 @@
 import { Typography, AppBar, CssBaseline, Container, Toolbar, Button } from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-//const navigate = useNavigate();
-//<Button onClick={()=>{navigate('/userInfo')}} color = "inherit"> <AccountCircle /> </Button>
 function AppBarr() {
+    const navigate = useNavigate();
     return (
         <AppBar position="relative" color="primary">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         WeBay
                     </Typography>
-                    <Button color = "inherit"> <AccountCircle /> </Button>
+                    <Button color = "inherit" onClick={()=>navigate('/userInfo')} > <AccountCircle /> </Button>
                 </Toolbar>
             </AppBar>
     )
