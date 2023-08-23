fa09c1e610afb46f43e109166c0f0a0989910925
dishika.goel <dishika.goel@drexel.edu>
2023-08-23 03:22:48 -0400

    Add a create new auction page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import Home from './pages/home';
 import UserInfo from './pages/userInfo';
 import Signup from './pages/signup';
 import Login from './pages/login';
+import NewAuction from './pages/newAuction.jsx';
 
 function App() {
-
     return (
         <Router>
             <div className='App'>
@@ -14,6 +14,7 @@ function App() {
                     <Route path="/userInfo" element={<UserInfo />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/newAuction" element={<NewAuction />} />
                 </Routes>
             </div>
         </Router>
