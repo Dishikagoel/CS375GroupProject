e948cd8a3fd5bce221c3d2e71638790dca28ed67
Shravya0 <81495955+Shravya0@users.noreply.github.com>
2023-08-29 13:16:06 -0400

    fix frontend login logic

--- server/app/routes/post.js
+++ server/app/routes/post.js
@@ -6,18 +6,19 @@ const pool = new Pool(env);
 
 router.post('/login', async (req, res) => {
     const { user, pass } = req.body;
+    console.log(user, pass);
 
     const query = 'SELECT * FROM userInfo WHERE userid = $1';
     const { rows } = await pool.query(query, [user]);
 
     if (rows.length === 0) {
-        return res.status(401).json({message: 'User not found', message: "Login successful"});
+        return res.status(401).json({message: 'User not found'});
     }
 
     const userDb = rows[0];
 
     if (pass === userDb.password) {
-        return res.json({userId: user});
+        return res.json({user: user, pass: pass});
     }
     res.status(401).json({message: 'Incorrect passowrd'});
 })
